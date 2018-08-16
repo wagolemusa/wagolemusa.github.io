7b83aab4bac886595f46cd3bd7725da65f359fa6
kisumutalent <homiemusa@gmail.com>
2018-08-16 19:28:06 +0300

    adding javascript

---
+++ js/index.js
@@ -0,0 +1,23 @@
+let route = "https://dairyapp.herokuapp.com/api/";
+
+function fetchIndex(){
+fetch(route+"/", {method:"GET", 
+headers:{"Content-Type":"application/json"}})
+.then((response) => response.json())
+.then((data)=>{
+    document.getElementById("output").innerHTML = data["message"]
+})
+.catch((err) => console.log(err))
+}
+
+
+
+// function getText(){
+// 	fetch('https://dairyapp.herokuapp.com/api/')
+// 	headers:{"Content-Type":"application/json"}
+// 	.then((res) => res.text())
+// 	.then((data) =>{
+// 		document.getElementById('output').innerHTML = data;
+// 	})
+// 	.catch((err) => console.log(err))
+// }
