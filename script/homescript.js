0e7115e68494bc20d13357f04e64576700b2c371
kisumutalent <homiemusa@gmail.com>
2018-08-17 15:37:24 +0300

    modify js

--- script/homescript.js
+++ script/homescript.js
@@ -11,24 +11,24 @@
 
 
 
-// let route = "https://kibitok.herokuapp.com/api/v2";
-// function fetchIndex(){
-// fetch(route+"/", {method:"GET", 
-// headers:{"Content-Type":"application/json"}})
-// .then((response) => response.json())
-// .then((data)=>{
-//     document.getElementById("output").innerHTML = data["message"]
-// })
-// .catch((err) => console.log(err))
-// }
-
-
-
-function getText(){
-	fetch('https://dairyapp.herokuapp.com/api/')
-	.then((res) => res.text())
-	.then((data) =>{
-		document.getElementById('output').innerHTML = data;
-	})
-	.catch((err) => console.log(err))
+let route = "https://dairyapp.herokuapp.com/api";
+function fetchIndex(){
+fetch(route+"/", {method:"GET", 
+headers:{"Content-Type":"application/json"}})
+.then((response) => response.json())
+.then((data)=>{
+    document.getElementById("output").innerHTML = data["message"]
+})
+.catch((err) => console.log(err))
 }
+
+
+
+// function getText(){
+// 	fetch('https://dairyapp.herokuapp.com/api/')
+// 	.then((res) => res.text())
+// 	.then((data) =>{
+// 		document.getElementById('output').innerHTML = data;
+// 	})
+// 	.catch((err) => console.log(err))
+// }
