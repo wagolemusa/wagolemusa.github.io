999be97e94e163f019440444c1868462102e77df
kisumutalent <homiemusa@gmail.com>
2018-09-09 00:21:01 +0300

    access token

--- script/homescript.js
+++ script/homescript.js
@@ -27,7 +27,7 @@ headers:{"Content-Type":"application/json"}})
 
 
 // register user
-token = document.cookie.split(',')[0];
+//token = document.cookie.split(',')[0];
 function fetchUser(){
 	event.preventDefault()
 	let url = route+"/v2/auth/signup"
@@ -79,84 +79,120 @@ function fetchUser(){
 }
 
 
-function fetchsession(){
-	event.preventDefault()
-	const url = route+"/v2/auth/signup";
-	fetch(url, {method: "GET",
-headers: {"Content-Type":"application/json", 'x-access-token':token}})
-.then((resp)=> resp.json())
-.then((data)=> {
-	if (data["message"] == "You are out of session" || data["message"] == "Your token expired Please Login again"
-|| data["message"] == "Invalid token please login to get a new token"){
-	window.location.replace('login.html');
-} else{console.log(data);}
-})
-.catch(error => console.log('error:',error));
-}
+// function fetchsession(){
+// 	event.preventDefault()
+// 	const url = route+"/v2/auth/signup";
+// 	fetch(url, {method: "GET",
+// headers: {"Content-Type":"application/json", 'x-access-token':token}})
+// .then((resp)=> resp.json())
+// .then((data)=> {
+// 	if (data["message"] == "You are out of session" || data["message"] == "Your token expired Please Login again"
+// || data["message"] == "Invalid token please login to get a new token"){
+// 	window.location.replace('login.html');
+// } else{console.log(data);}
+// })
+// .catch(error => console.log('error:',error));
+// }
 
 
-function onload(){
-	const url = route+"/v2/auth/signup"
-	fetch(url, {method:"GET", 
-headers: {"Content-Type":"application/json", 'x-access-token':token}})
-.then((resp)=>resp.json())
-.then((data) =>{
-    if (data["message"] == "you are out of session" || data["message"] == "your token expired please login again"
-|| data["message"] == "invalid token please login to get a new token")
-{
-window.location.replace('login.html');
-} 
-else{console.log("Welcome "+data["name"]);
+// function onload(){
+// 	const url = route+"/v2/auth/signup"
+// 	fetch(url, {method:"GET", 
+// headers: {"Content-Type":"application/json", 'x-access-token':token}})
+// .then((resp)=>resp.json())
+// .then((data) =>{
+//     if (data["message"] == "you are out of session" || data["message"] == "your token expired please login again"
+// || data["message"] == "invalid token please login to get a new token")
+// {
+// window.location.replace('login.html');
+// } 
+// else{console.log("Welcome "+data["name"]);
 
-}
+// }
 
-})
-.catch(error =>{ window.location.replace('add_entry.html');});
-}
+// })
+// .catch(error =>{ window.location.replace('add_entry.html');});
+// }
 
 
 
-function fetchlogin(){
-	event.preventDefault()
-	let username = document.forms["login"]["username"].value;
-	let password = document.forms["login"]["password"].value;
+// function fetchlogin(){
+// 	event.preventDefault()
+// 	let username = document.forms["login"]["username"].value;
+// 	let password = document.forms["login"]["password"].value;
 
-	if (username == ""){
-		alert("Username can't not be empty");
-		document.login.username.focus();
-		return false
-	}
-	else if (password == ""){
-		alert("Password can't be empity");
-		document.login.password.focus();
-		return false
-	}
-	else{
-		let url = route+"/v2/auth/login"
+// 	if (username == ""){
+// 		alert("Username can't not be empty");
+// 		document.login.username.focus();
+// 		return false
+// 	}
+// 	else if (password == ""){
+// 		alert("Password can't be empity");
+// 		document.login.password.focus();
+// 		return false
+// 	}
+// 	else{
+// 		let url = route+"/v2/auth/login"
 
-		let data = {username:username, password:password};
-		fetch(url, {method: "POST",
-		headers:{
-			"Content-Type":"application/json"
-		},
-	body:JSON.stringify(data)
-	})
-	.then((res)=>res.json())
-	.then((data)=> {
-		if (data["token"]){
-			let date = new Date();
-			date.setTime(date.getTime()+(5000*60*60*30));
-			document.cookie = data["token"]+"; expires="+date.toGMTString();
-			window.location.replace("add_entry.html");
-		}
-		else {
-			document.getElementById("regstatus").innerText = data["message"];
-			console.log(data["message"]);
-		}
-	})
-	.catch(error => console.log('error:',error));
-	}
-}
+// 		let data = {username:username, password:password};
+// 		fetch(url, {method: "POST",
+// 		headers:{
+// 			"Content-Type":"application/json"
+// 		},
+// 	body:JSON.stringify(data)
+// 	})
+// 	.then((res)=>res.json())
+// 	.then((data)=> {
+// 		if (data["token"]){
+// 			let date = new Date();
+// 			date.setTime(date.getTime()+(5000*60*60*30));
+// 			document.cookie = data["token"]+"; expires="+date.toGMTString();
+// 			window.location.replace("add_entry.html");
+// 		}
+// 		else {
+// 			document.getElementById("regstatus").innerText = data["message"];
+// 			console.log(data["message"]);
+// 		}
+// 	})
+// 	.catch(error => console.log('error:',error));
+// 	}
+// }
+
+document.getElementById("LoginForm").addEventListener("submit", function (event) {
+	event.preventDefault();
+	const username = document.getElementById("username");
+	const password = document.getElementById("password");
+  
+	  credentials = {
+		  username: username.value,
+		  password: password.value
+	  };
+	  fetch("https://dairyapp.herokuapp.com/api/v2/auth/login", {
+			  method: "POST",
+			  headers: {
+				  "Content-Type": "application/json"
+			  },
+			  body: JSON.stringify(credentials)
+		  })
+		  .then((response)=>{
+			response.json().then((data) => {
+			  if (data["message"]["token"]){
+				let token = data["message"]["token"]
+				localStorage.setItem('token', JSON.stringify(token));
+				window.location.replace("diary_notes.html");
+			  }
+			  else{
+				console.log(data["message"]);
+				//console.log(data)
+				const RegResponse = Object(data.message)
+				let Message = document.getElementById("logResponse");
+				const FetchedMessage = `<p class"res">${RegResponse}</p>`
+				Message.innerHTML = FetchedMessage
+			  } 
+		  })
+		  .catch(err => console.log(err));
+  })});
+
 
 
 function fetchAddEntry(){
