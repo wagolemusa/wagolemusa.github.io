346532d18b8a3019702bbea63cc7fff25bdd1935
kisumutalent <homiemusa@gmail.com>
2018-09-10 19:25:14 +0300

    add view button

--- script/viewupdates.js
+++ script/viewupdates.js
@@ -21,6 +21,8 @@ fetch("https://dairyapp.herokuapp.com/api/v2/entries",{
 			window.location.replace(login.html)
 		}
 		else{
+			data = data["data"];
+			//console.log(data)
 			let output =`
 			<div style="overflow-x:auto;">
 			<table>
@@ -28,25 +30,26 @@ fetch("https://dairyapp.herokuapp.com/api/v2/entries",{
 			<th>Title</th>
 			<th>Date</th>
 			<th></th>
-
 			<th>Action</th>
-
 			</tr>`;
 			Object.keys(data).forEach(function(entr){
 				//let entries = "`"+data[entr]["entries"]+"`";
 				let title = data[entr]["title"];
 				let dates = data[entr]["dates"];
 				let entries = data[entr]["entries"];
-				console.log(data)
+				//console.log(data)
 
 				output += `
 				<tr>
+
 				<td>${data[entr]["title"]}</td>
 			
 				<td>${data[entr]["dates"]}</td>
-				<td><div id="myBtn"class="view", onclick="viewSingle(${data[entr]["entry_id"]})">
-				<td> <div id="Btn" class="view", onclick="modifyEntry(${data[entr]["entry_id"]}','${data[entr]["title"]}',${entries})"></div>
-				<h5>EDIT</h5></div>  <div class="view" onclick="deleteEntry(${data[entr]["entry_id"]})"><h5>DELETE</h5></div></td>
+				<td><input type="submit" id="myBtn" value="view" onclick="viewSingle(${data[entr]["entry_id"]})"></div></td>
+				<td><input type="submit" id="myBtn1" value="edit" onclick="editPost('${data[entr]["entry_id"]}','${data[entr]["title"]}',${entries})"></td>
+				<td> <div id="Btn" class="view", onclick="modifyEntry(${data[entr]["entry_id"]}','${data[entr]["title"]}',${entries})"></td>
+				<td><input type="submit" id="delete" value="delete" onclick="deleteEntry(${data[entr]["entry_id"]})"></td>
+                
 				</tr>`;
 			});
 			document.getElementById("tableform").innerHTML = output + `</table></div>`;
@@ -54,6 +57,63 @@ fetch("https://dairyapp.herokuapp.com/api/v2/entries",{
 	})
 	.catch(err => console.log(err));
 })
+
+
+
+// let route2 = "https://dairyapp.herokuapp.com/api";
+
+
+// let token = JSON.parse(localStorage.getItem("token"));
+// let modal = document.getElementById('myModal');
+// let btn = document.getElementById("myBtn");
+// let span = document.getElementsByClassName("close")[0];
+
+// fetch("https://dairyapp.herokuapp.com/api/v2/entries",{
+// 	method: "GET",
+// 	headers: {
+// 		"Content-Type": "application/json",
+// 		"Accept":"application/json",
+// 		"x-access-token":token
+// 	},
+// })
+// .then((response)=>{
+// 	response.json().then((data)=>{ 
+// 		//console.log(data)
+// 		if (data["message"] == "Token is missing!" || data["message"] == "Token is invalid!"){
+// 			window.location.replace(login.html)
+// 		}
+// 		else{
+// 			let output =`
+// 			<div style="overflow-x:auto;">
+// 			<table>
+// 			<tr>
+// 			<th>Title</th>
+// 			<th>Date</th>
+// 			<th></th>
+// 			<th>Action</th>
+// 			</tr>`;
+// 			data.forEach(function(entr){
+// 				//let entries = "`"+data[entr]["entries"]+"`";
+// 				 let title = entr["title"];
+// 				 let dates = entr["dates"];
+// 				// let entries = data[entr]["entries"];
+// 				console.log(entr)
+
+// 				output += `
+// 				<tr>
+// 				<td>${entr.title}</td>
+			
+// 				<td>${entr.dates}</td>
+// 				<td><div id="myBtn"class="view", onclick="viewSingle(${entr.entry_id})">
+// 				<td> <div id="Btn" class="view", onclick="modifyEntry(${entr.entry_id}','${entr.title}',${entr.entries})"></div>
+// 				<h5>EDIT</h5></div>  <div class="view" onclick="deleteEntry(${entr.entry_id})"><h5>DELETE</h5></div></td>
+// 				</tr>`;
+// 			});
+// 			document.getElementById("tableform").innerHTML = output + `</table></div>`;
+// 		}
+// 	})
+// 	//.catch(err => console.log(err));
+// })
 
 // let token = JSON.parse(localStorage.getItem("token"));
 // let modal = document.getElementById('myModal');
@@ -68,7 +128,7 @@ fetch("https://dairyapp.herokuapp.com/api/v2/entries",{
 //         let url = route2+"/v2/entries";
 //         fetch(url, {
 //             method:"GET",
-//             headers: {"Content-Type":"application/json", 'X-API-KEY':token}
+//             headers: {"Content-Type":"application/json", 'x-access-token':token}
 //         })
 //         .then((response) => response.json())
 //         .then((data) => {
@@ -84,16 +144,15 @@ fetch("https://dairyapp.herokuapp.com/api/v2/entries",{
 //                 <tr>
 // 				<th>Title</th>
 // 				<th>Date</th>
-
 //                 <th>Action</th>
-//                 </tr>
-//                 `;
+//                 </tr>`;
 //                 Object.keys(data["message"]).forEach(function(ent){
-                    // let title = data["message"][ent]["title"];
-                    // let dates = data["message"][ent]["dates"];
-                    // //let entries = data["message"][ent]["entries"];
+//                     let title = data["message"][ent]["title"];
+//                     let dates = data["message"][ent]["dates"];
+// 					//let entries = data["message"][ent]["entries"];
+// 					console.log(data)
 
-//                     output += `
+//                     output +=`
 //                     <tr>
 //                     <td>${data["message"][ent]["title"]}</td>
 //                     <td>${data["message"][ent]["dates"]}</td>
@@ -101,8 +160,7 @@ fetch("https://dairyapp.herokuapp.com/api/v2/entries",{
 //                     ${data["message"][ent]["title"]}</td>
 //                     <td> <div id="Btn" class="view", onclick="edit(${data["message"][ent]["ID"]},\`${title}\`,\`${dates}\`)">
 //                     <h5>EDIT</h5></div>  <div class="view" onclick="deletes(${data["message"][ent]["ID"]})"><h5>DELETE</h5></div></td>
-//                     </tr>
-//                     `;
+//                     </tr>`;
 //                 });
 //                 document.getElementById("tableform").innerHTML = output + `</table></div>`;
 //                 document.getElementById("total").innerHTML = "Total entries : " +Object.keys(data["message"]).length.toString();
