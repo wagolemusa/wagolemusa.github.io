cbd3527e3b3b6223e77333b03cc4b3adfcd03652
kisumutalent <homiemusa@gmail.com>
2020-03-05 12:36:07 +0300

    Modify fetch price location

--- UI/js/create_price.js
+++ UI/js/create_price.js
@@ -1,9 +1,4 @@
 
- $ ('#datepicker').datepicker({
-    weekdaysShort: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
-    showMonthsShort: true
-    })
-
 let token = localStorage.getItem('access_token')
 let current_user = localStorage.getItem('current_user')
 let access_token = "Bearer " + token
